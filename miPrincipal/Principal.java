52daa2a1459e45a1f36defada0c0680cbf956e6e
Jair Brandu Gutierrez Hernandez <137021856+jairbrandu@users.noreply.github.com>
2023-06-23 11:12:25 -0700

    Listo

--- miPrincipal/Principal.java
+++ miPrincipal/Principal.java
@@ -1,11 +1,27 @@
 package miPrincipal;
 
-public class Principal {
-    public String getGreeting() {
-        return "Hello World!";
-    }
+import java.util.Scanner;
 
+public class Principal {
+    
     public static void main(String[] args) {
-        System.out.println(new Principal().getGreeting());
+        Calculadora c = new Calculadora();
+        int x,y;
+
+        Scanner consola = new Scanner(System.in);
+
+        System.out.println("Calculadora aritmetica");
+        System.out.println("======================");
+
+        System.out.print("Introduce el primer operando:");
+        x = consola.nextInt();
+        
+        System.out.print("Introduce el segundo operando:");
+        y = consola.nextInt();
+
+        System.out.println(x+"+"+y+"="+c.suma(x, y));
+        
+
+        consola.close();
     }
 }
